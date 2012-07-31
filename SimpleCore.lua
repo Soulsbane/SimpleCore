5c1c5a3d5b9c546a8b2d9024b5f9df8fea0dfb00
Paul Crane <paul@acheronsoft.com>
2012-07-30 22:08:36 -0500

    Add Print function and a few other variables we'll need later.

--- SimpleCore.lua
+++ SimpleCore.lua
@@ -1,1 +1,15 @@
- 
+local AddonName, Addon = ...
+local AddonFrame = CreateFrame("Frame", AddonName .. "AddonFrame", UIParent)
+
+local PRINTHEADER = "|cff33ff99" .. AddonName .. "|r: "
+
+AddonFrame:RegisterEvent("PLAYER_LOGIN")
+AddonFrame:RegisterEvent("PLAYER_LOGOUT")
+AddonFrame:RegisterEvent("ADDON_LOADED")
+
+---------------------------------------
+-- Utility Functions 
+---------------------------------------
+function Addon:Print(...)
+	print(PRINTHEADER, string.format(...))
+end
