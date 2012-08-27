463a3000de7bf1539be9f2582ae8c287c2257e8b
Paul Crane <paul@acheronsoft.com>
2012-08-26 20:36:26 -0500

    Change Registering events to OnEnable functions in Targets module.

--- Modules/Targets.lua
+++ Modules/Targets.lua
@@ -2,6 +2,9 @@ local AddonName, Addon = ...
 local Module = Addon:NewModule("Targets")
 
 function Module:OnInitialize()
+end
+
+function Module:OnEnable()
 	self:RegisterEvent("PLAYER_TARGET_CHANGED")
 end
 
