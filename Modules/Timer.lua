ab86830178ddb43d3c5c5df38259851a0eec92fc
Paul Crane <paul@acheronsoft.com>
2012-10-14 00:29:00 -0500

    Add test code for non-repeating timer in Timer module.

--- Modules/Timer.lua
+++ Modules/Timer.lua
@@ -1,1 +1,10 @@
- 
+local AddonName, Addon = ...
+local Module = Addon:NewModule("Timer")
+
+function Module:OnInitialize()
+	self:SetTimer(30, "NonRepeatingTimer", false)
+end
+
+function Module:NonRepeatingTimer(elapsed)
+	self:Print("NonRepeatingTimer test....")
+end
